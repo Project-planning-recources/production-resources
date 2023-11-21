7669d6d558b9e9ac4c69344c7289d1620a67e27f
AlexeyBaldin <kret906alex@gmail.com>
2023-11-21 16:46:19 +0300

    Подготовка тестера параллельных алгоритмов

--- src/main/java/util/ParallelTester.java
+++ src/main/java/util/ParallelTester.java
@@ -9,6 +9,7 @@ import testing.RealityTester;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ParallelTester {
     public static void main(String[] args) {
@@ -16,12 +17,12 @@ public class ParallelTester {
         int threadsNum = 4;
         int startGen = 10;
         int budgetGen = 100;
-        int startsNum = 10;
+        int startsAlg = 10;
         int basisSize = 5;
 
-        try (FileWriter writer = new FileWriter("parallel.csv", false)) {
-            writer.write("№;Количество заказов;Среднее количество операций;Количество атомарных ресурсов;Минимальное число альтернатив на деталь;" +
-                    "Максимальное число альтернатив на деталь;Среднее число альтернатив на деталь;Среднее суммарное количество дней просрочки;Средний критерий;Среднее время исполнения в секундах\n");
+        try (FileWriter writer = new FileWriter(argv[6], false)) {
+            writer.write("№;Количество заказов;Количество типов деталей;Среднее количество деталей каждого типа;Среднее количество операций на деталь;Количество атомарных ресурсов;Минимальное число альтернатив на деталь;" +
+                    "Максимальное число альтернатив на деталь;Среднее число альтернатив на деталь;Количество произведенных операций;Среднее суммарное количество дней просрочки;Средний критерий;Среднее время исполнения в секундах\n");
 
             for (int i = 0; i < basisSize; i++) {
                 InputProduction production = READER.readProductionFile(argv[2] + "/" + (i + 1) + "_production.xml");
@@ -34,7 +35,7 @@ public class ParallelTester {
                     Data.AlternativenessCount alternativenessCount = Data.getAlternativenessCount(orders.getOrders());
                     long equipmentCount = Data.getEquipmentCount(production);
 
-                    long operationsCount = 0;
+                    long performOperationsCount = 0;
                     long averageOverdueDays = 0;
                     double averageCriterion = 0;
                     long averageTime = 0;
@@ -50,7 +51,7 @@ public class ParallelTester {
                         if (RealityTester.test(production, orders, ownResult)) {
                             long endTime = System.currentTimeMillis();
 
-                            operationsCount += Data.getOperationsCount(ownResult);
+                            performOperationsCount += Data.getPerformOperationsCount(ownResult);
                             averageOverdueDays += Data.getAverageOverdueDays(orders.getOrders(), ownResult);
                             averageCriterion += Criterion.getCriterion(orders, ownResult);
                             averageTime += (endTime - startTime) / 1000;
@@ -63,11 +64,14 @@ public class ParallelTester {
 
                     writer.write((i + 1) + ";" +
                             orders.getOrders().size() + ";" +
-                            ((double) operationsCount / startsAlg) + ";" +
+                            Data.getDetailTypesCount(orders.getOrders()) + ";" +
+                            Data.getAverageDetailsCount(orders.getOrders()) + ";" +
+                            Data.getAverageOperationsCountOnDetail(orders.getOrders()) + ";" +
                             equipmentCount + ";" +
                             alternativenessCount.min + ";" +
                             alternativenessCount.max + ";" +
                             alternativenessCount.average + ";" +
+                            ((double) performOperationsCount / startsAlg) + ";" +
                             ((double) averageOverdueDays / startsAlg) + ";" +
                             (averageCriterion / startsAlg) + ";" +
                             ((double) averageTime / startsAlg) + "\n");
@@ -76,8 +80,6 @@ public class ParallelTester {
                 }
             }
             System.out.println("Работа завершена.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
