fdcb251e452d52e976f3be8945c72b4925f31106
AlexeyBaldin <kret906alex@gmail.com>
2023-11-21 15:50:41 +0300

    New branch for parallel alternativeness algorithm

---
+++ src/main/java/util/ParallelTester.java
@@ -0,0 +1,83 @@
+package util;
+
+import algorithm.AlternativenessOwnAlgorithm;
+import parse.input.order.InputOrderInformation;
+import parse.input.production.InputProduction;
+import parse.output.result.OutputResult;
+import testing.PossibilityTester;
+import testing.RealityTester;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ParallelTester {
+    public static void main(String[] args) {
+
+        int threadsNum = 4;
+        int startGen = 10;
+        int budgetGen = 100;
+        int startsNum = 10;
+        int basisSize = 5;
+
+        try (FileWriter writer = new FileWriter("parallel.csv", false)) {
+            writer.write("№;Количество заказов;Среднее количество операций;Количество атомарных ресурсов;Минимальное число альтернатив на деталь;" +
+                    "Максимальное число альтернатив на деталь;Среднее число альтернатив на деталь;Среднее суммарное количество дней просрочки;Средний критерий;Среднее время исполнения в секундах\n");
+
+            for (int i = 0; i < basisSize; i++) {
+                InputProduction production = READER.readProductionFile(argv[2] + "/" + (i + 1) + "_production.xml");
+                InputOrderInformation orders = READER.readOrderFile(argv[2] + "/" + (i + 1) + "_orders.xml");
+
+                if (PossibilityTester.test(production, orders)) {
+//                            BaseAlgorithm baseAlgorithm = new BaseAlgorithm(production, orders.getOrders(), null);
+//                            OutputResult baseResult = baseAlgorithm.start();
+
+                    Data.AlternativenessCount alternativenessCount = Data.getAlternativenessCount(orders.getOrders());
+                    long equipmentCount = Data.getEquipmentCount(production);
+
+                    long operationsCount = 0;
+                    long averageOverdueDays = 0;
+                    double averageCriterion = 0;
+                    long averageTime = 0;
+
+                    for (int j = 0; j < startsAlg; j++) {
+
+                        System.out.println(i + ":" + j + ": Запущен...");
+
+                        long startTime = System.currentTimeMillis();
+                        AlternativenessOwnAlgorithm ownAlgorithm = new AlternativenessOwnAlgorithm(production, orders.getOrders(), null, startGen, budgetGen);
+                        OutputResult ownResult = ownAlgorithm.start();
+
+                        if (RealityTester.test(production, orders, ownResult)) {
+                            long endTime = System.currentTimeMillis();
+
+                            operationsCount += Data.getOperationsCount(ownResult);
+                            averageOverdueDays += Data.getAverageOverdueDays(orders.getOrders(), ownResult);
+                            averageCriterion += Criterion.getCriterion(orders, ownResult);
+                            averageTime += (endTime - startTime) / 1000;
+
+                            System.out.println(i + ":" + j + ": Завершён...");
+                        } else {
+                            throw new Exception(i + ":" + j + ": Результат собственного алгоритма не соответствует заказам");
+                        }
+                    }
+
+                    writer.write((i + 1) + ";" +
+                            orders.getOrders().size() + ";" +
+                            ((double) operationsCount / startsAlg) + ";" +
+                            equipmentCount + ";" +
+                            alternativenessCount.min + ";" +
+                            alternativenessCount.max + ";" +
+                            alternativenessCount.average + ";" +
+                            ((double) averageOverdueDays / startsAlg) + ";" +
+                            (averageCriterion / startsAlg) + ";" +
+                            ((double) averageTime / startsAlg) + "\n");
+                } else {
+                    throw new Exception(i + ": Заказы не соответствуют производству");
+                }
+            }
+            System.out.println("Работа завершена.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
